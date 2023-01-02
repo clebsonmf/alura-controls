066d59358251741e0d016894065cc7f14804025a
Clebson Morais <clebson.cont@gmail.com>
2023-01-02 10:11:47 -0300

    Adicionado o botão de excluir um projeto

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -1,6 +1,5 @@
 <template>
-    <section class="projetos">
-        <h1 class="title">Projetos</h1>
+    <section>
         <form @submit.prevent="salvar">
             <div class="field">
                 <label for="nomeDoProjeto" class="label">Nome do Projeto</label>
@@ -23,6 +22,7 @@
 <script lang="ts">
 import { defineComponent} from 'vue';
 import { useStore } from '../../store';
+import { ADICIONA_PROJETO, ALTERA_PROJETO} from "../../store/tipo-mutacoes";
 export default defineComponent ({
     name: 'FormularioVue',
     props: {
@@ -44,12 +44,12 @@ export default defineComponent ({
     methods: {
         salvar (){
             if (this.id) {
-                this.store.commit('ALTERA_PROJETO', {
+                this.store.commit(ALTERA_PROJETO, {
                     id: this.id,
-                    nome: this.nomeDoProjeto
+                    nome: this.nomeDoProjeto 
                 })
             } else {
-                this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+                this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto='',
             this.$router.push('/projetos')
@@ -64,8 +64,3 @@ export default defineComponent ({
 });
 </script>
 
-<style scoped>
-.projetos {
-    padding: 1.5rem
-}
-</style>
